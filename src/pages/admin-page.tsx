48771bb9f174a8285d99550d96bfa8a5ee7eb005
Aviv <aviv80010@gmail.com>
2022-08-02 17:08:13 +0300

    Made basic user table for admin page, you can now delete users as an admin

--- src/pages/admin-page.tsx
+++ src/pages/admin-page.tsx
@@ -19,10 +19,45 @@ export const AdminPage = () => {
     setUsers(users)
   }
 
+  const onDeleteUser = async (userId: string) => {
+    await userService.remove(userId)
+    loadUsers()
+  }
+
   return (
-    <section className="admin-page">
+    <section className="admin-page full-screen flex column align-center">
       <h1>Admin dashboard</h1>
-      <UserList users={users} />
+      <table className="users-table">
+        <thead>
+          <tr>
+            <th className="column1">User avatar</th>
+            <th>Full name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user: User) => {
+            if (user.role === 'admin') return
+            return (
+              <tr key={user._id} className="user-info-row">
+                <td className="avatar column1">
+                  <img src={user.avatar} alt="" />
+                </td>
+                <td>{user.fullname}</td>
+                <td className="actions">
+                  <button className="btn-edit">Edit</button>
+                  <button
+                    onClick={() => onDeleteUser(user._id)}
+                    className="btn-delete"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </section>
   )
 }
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,14 +1,27 @@
-import { useEffect } from 'react'
+import { UserList } from 'cmps/user-list'
+import { User } from 'models/user.model'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { userService } from 'services/user.service'
 
 export function HomePage() {
   const user = userService.getLoggedinUser()
+  const [users, setUsers] = useState([] as User[])
+
   const navigate = useNavigate()
 
   useEffect(() => {
     if (!user) navigate('/')
+    loadUsers()
   }, [])
 
-  return <section className="home-page"></section>
+  const loadUsers = async () => {
+    const users = await userService.getUsers()
+    setUsers(users)
+  }
+  return (
+    <section className="home-page">
+      <UserList users={users} />
+    </section>
+  )
 }
